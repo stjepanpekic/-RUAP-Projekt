8d77a6b9ed776664d0c95e8142ec3e9c8284cdca
stjepanpekic <peka1607@gmail.com>
2024-11-05 12:36:32 +0100

    add index number to message

--- Consoleapp/ConsoleApp1/Program.cs
+++ Consoleapp/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Stjepan Pekić");
+            Console.WriteLine("Pozdrav iz aplikacije od Stjepan Pekić, datum rođenja- 16.07.2000.");
         }
     }
 }
